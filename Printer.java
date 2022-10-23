1bb3a364d262287eb924de0b7b9cfd5a99eee9ac
Alexis <57765568+alekzihz@users.noreply.github.com>
2022-10-24 01:39:44 +0200

    nueva variable DateTimeFormatter para dar formato a las fechas.

--- Printer.java
+++ Printer.java
@@ -1,7 +1,12 @@
+import java.time.format.DateTimeFormatter;
 import java.util.Observable;
 import java.util.Observer;
 
 public class Printer implements Visitor, Observer {
+    //private static final DateTimeFormatter DATEFORMATTER;
+    private static final DateTimeFormatter DATEFORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+
 
     Project tree;
     public Printer(Project root) {
@@ -19,9 +24,11 @@ public class Printer implements Visitor, Observer {
     @Override
     public void visitProject(Project project) {
 
-        String formatString = String.format("Project: %20s %40s %40s %20s", project.getTagName(),project.getInitialDate(),project.getDateFinal(),
-                project.getDuration().toSeconds());
-        System.out.println(formatString);
+
+            String formatString = String.format("Project : %15s %30s %30s %20s", project.getTagName(),(project.getInitialDate()).format(DATEFORMATTER),(project.getDateFinal()).format(DATEFORMATTER),
+                    project.getDuration().toSeconds());
+            System.out.println(formatString);
+
 
 
         //System.out.println("Project "+ project.getTagName() + " child of "+project.getParentTagName()+"%15s" +project.getInitialDate()+"%15s"+project.getFinalDate()+ " "
@@ -31,11 +38,11 @@ public class Printer implements Visitor, Observer {
     }
     @Override
     public void visitTask(Task task) {
-        String formatString = String.format("Task:    %20s %40s %40s %20s", task.getTagName(),task.getInitialDate(),task.getDateFinal(),
-                task.getDuration().toSeconds());
-        System.out.println(formatString);
 
 
+            String formatString = String.format("activity: %15s %30s %30s %20s", task.getTagName(),task.getInitialDate().format(DATEFORMATTER),task.getDateFinal().format(DATEFORMATTER),
+                    task.getDuration().toSeconds());
+            System.out.println(formatString);
 
         //System.out.println("Task " + task.getTagName() + " child of " + task.getParentProject().getTagName() + " " + task.getInitialDate() + " " + task.getFinalDate() + " "
         //        + task.getDurationSeconds());
@@ -46,7 +53,7 @@ public class Printer implements Visitor, Observer {
     @Override
     public void visitInterval(Interval interval) {
 
-        String formatString = String.format("Interval:%20s       child of %4s %40s %40s %20s", interval.getTask().getTagName(),interval.getTask().getParentTagName(),interval.getInitialDate(),interval.getFinalDate(),
+        String formatString = String.format("Interval:   %44s %30s %20s", interval.getInitialDate().format(DATEFORMATTER),interval.getFinalDate().format(DATEFORMATTER),
                 interval.getDuration().toSeconds());
         System.out.println(formatString);
 
@@ -56,7 +63,7 @@ public class Printer implements Visitor, Observer {
     public void update(Observable o, Object arg) {
         //System.out.println(LocalDateTime.now());
         tree.acceptVisitor(this);
-        //this.visitProject(tree);
+        this.visitProject(tree);
 
     }
 }
