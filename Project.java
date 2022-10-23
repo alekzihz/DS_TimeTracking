f22282f7bffb1e9c541ed23b09e4942755089deb
Alexis <57765568+alekzihz@users.noreply.github.com>
2022-10-24 01:46:00 +0200

    actualizacion metodos:  -updateFinalDate  -updateDuration se añadio metodo  -updateInitialDate

--- Project.java
+++ Project.java
@@ -15,28 +15,52 @@ public class Project extends Component{
     @Override
     void updateFinalDate(LocalDateTime newFinalDate) {
         setDateFinal(newFinalDate);
-        if(this.tagName!="root"){
-            parentProject.setDateFinal(newFinalDate);
+        if(this.parentProject!=null){
+            parentProject.updateFinalDate(newFinalDate);
         }
+    }
+
+    void updateInitialDate(LocalDateTime InitialDate){
+        if(this.initialDate==null){
+            setInitialDate(InitialDate);
+        }
+        if(this.parentProject!=null){
+            this.parentProject.updateInitialDate(initialDate);
+        }
+
     }
 
     @Override
     void updateDuration(Duration newDuration, Time newTimer) {
-        setDuration(newDuration);
-        if(this.tagName!="root"){
-            parentProject.setDuration(newDuration);
-        }
-        /*if(this.duration.getSeconds()==0){
+
+        Printer pi = new Printer(this);
+
+        //si duracion es 0 set newduration
+        if(this.duration.getSeconds()==0){
             this.setDuration(newDuration);
-        }else{
-            this.duration=this.duration.plusSeconds(newTimer.getSeconds());
-            //this.parentProject.setDuration(duration);
-            this.setDuration(duration);
+            pi.visitProject(this);
         }
-        if(this.parentProject!=null){
-            this.parentProject.duration=this.parentProject.duration.plusSeconds(newTimer.getSeconds());
-            this.parentProject.setDuration(this.parentProject.duration);
-        }*/
+        //mientras sea diferente de 0 sumar la duracion anterior con el ciclo del reloj(2).
+        else{
+                this.duration=this.duration.plusSeconds(newTimer.getSeconds());
+                this.setDuration(this.duration);
+                pi.visitProject(this);
+        }
+
+        //mientras el proyecto tenga un padre actualizar las duracions.
+
+        if(this.parentProject!=null) {
+            //si el proyectoPadre es root y la duracion del proyecto actual sea 0 la duracion del proyecto root + newDuration
+            if((this.getParentProject().getTagName()).equals("root") && this.duration.getSeconds()==0){
+                this.parentProject.duration=this.parentProject.duration.plusSeconds(newDuration.getSeconds());
+                this.setDuration(this.parentProject.duration);
+            }else{
+
+                this.parentProject.updateDuration(this.duration, newTimer);
+
+            }
+
+        }
     }
 
     @Override
@@ -46,15 +70,12 @@ public class Project extends Component{
             i.acceptVisitor(v);
         }
     }
-
     public List<Component> getChildrenProject() {
         return childrenProject;
     }
-
     public void setChildrenProject(List<Component> childrenProject) {
         this.childrenProject = childrenProject;
     }
-
     protected void addComponent(Component component){
         childrenProject.add(component);
         component.setParentProject(this);
