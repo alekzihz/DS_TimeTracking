1face4d090acb2d30aa231940dc92f51d50d971d
Alexis <57765568+alekzihz@users.noreply.github.com>
2022-10-29 10:50:28 +0200

    Implementacion de Apendice A y B

--- Main.java
+++ Main.java
@@ -5,6 +5,12 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
+        //apendiceA();
+        apendiceB();
+
+
+    }
+    public static void apendiceA(){
         Project root = new Project("root");
         Project softwareDesign= new Project("software design",root);
         Project softwareTesting = new Project("sotware testing",root);
@@ -29,6 +35,31 @@ public class Main {
         timeTracker.addComponent(firstMilestone);
 
 
+
+    }
+    public static void apendiceB() throws InterruptedException {
+        Project root = new Project("root");
+        Project softwareDesign= new Project("software design",root);
+        Project softwareTesting = new Project("sotware testing",root);
+        Project dataBase = new Project("database", root);
+        Task transportation = new Task("transpotation", root);
+        Project Problems = new Project("problems", softwareDesign);
+        Project timeTracker = new Project("time tracker", softwareDesign);
+        Task firstList = new Task("first list", Problems);
+        Task secondList = new Task("second list", Problems);
+        Task readHandout = new Task("read handout", timeTracker);
+        Task firstMilestone = new Task("first milestone", timeTracker);
+
+        root.addComponent(softwareDesign);
+        root.addComponent(softwareTesting);
+        root.addComponent(dataBase);
+        root.addComponent(transportation);
+        softwareDesign.addComponent(Problems);
+        softwareDesign.addComponent(timeTracker);
+        Problems.addComponent(firstList);
+        Problems.addComponent(secondList);
+        timeTracker.addComponent(readHandout);
+        timeTracker.addComponent(firstMilestone);
         Time clock = Time.getIntanceTime(2);
         //Printer printer = new Printer(root);
         //clock.addObserver(printer);
