5a04e8d329778702b2a194d3db8e4c64e7dcebf5
Alexis <57765568+alekzihz@users.noreply.github.com>
2022-11-01 19:01:21 +0100

    Funciones:  -readJeson  -creationRootfromJson  -feedChildren  -feedInterval

--- Main.java
+++ Main.java
@@ -1,24 +1,36 @@
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+
+//importa org.json.parser;
+
+import java.io.FileReader;
+import java.time.Duration;
+import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 
 import static java.lang.System.exit;
 import static java.lang.Thread.sleep;
 
 public class Main {
-    public static Project root = new Project("root");
-    public static void main(String[] args) throws InterruptedException {
+    public static Project root = null;
+    public static void main(String[] args) throws InterruptedException, FileNotFoundException {
 
         //apendiceA();
-        apendiceB();
-        crearJSON();
+        //apendiceB();
+        //crearJSON();
+
+
+        readJson();
         exit(0);
     }
     public static void apendiceA(){
+        root = new Project("root");
         Project softwareDesign= new Project("software design",root);
         Project softwareTesting = new Project("sotware testing",root);
         Project dataBase = new Project("database", root);
@@ -45,6 +57,8 @@ public class Main {
 
     }
     public static void apendiceB() throws InterruptedException {
+
+        root = new Project("root");
         Project softwareDesign= new Project("software design",root);
         Project softwareTesting = new Project("sotware testing",root);
         Project dataBase = new Project("database", root);
@@ -102,6 +116,140 @@ public class Main {
     //ArrayList<Task> task = new ArrayList<Task>();
     //todo: corregir actualizacion duracion
 
+    public static void readJson() throws FileNotFoundException {
+        String resourceName = "file.json";
+
+
+
+        InputStream is = Main.class.getResourceAsStream(resourceName);
+        if (is == null) {
+            throw new NullPointerException("Cannot find resource file " + resourceName);
+        }
+
+        JSONTokener tokener = new JSONTokener(is);
+        JSONObject object = new JSONObject(tokener);
+        creatingRootfromJson(object);
+
+        //root.setDuration(Duration.ofSeconds(2));
+
+
+    }
+
+    private static void creatingRootfromJson(JSONObject object) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        root = new Project("root");
+        root.setDuration(Duration.ofSeconds(object.getJSONObject("root").getInt("Duration")));
+        root.setInitialDate(LocalDateTime.parse(object.getJSONObject("root").getString("InitialDate"), formatter));
+        root.setDateFinal((LocalDateTime.parse(object.getJSONObject("root").getString("FinalDate"), formatter)));
+        //root.setTagName(object.getJSONObject("root").getString("Name"));
+
+
+        JSONArray children = object.getJSONObject("root").getJSONArray("children");
+        List<Component> listChildren = new ArrayList<>();
+
+        for (int i = 0; i < children.length(); i++) {
+            JSONObject obj = children.getJSONObject(i);
+            Component comp = null;
+            comp = feedChildren(obj, comp);
+            listChildren.add(comp);
+            root.addComponent(comp);
+        }
+
+        root.setChildrenProject(listChildren);
+    }
+
+    /*
+
+        root = new Project("root");
+        Project softwareDesign= new Project("software design",root);
+        Project softwareTesting = new Project("sotware testing",root);
+        Project dataBase = new Project("database", root);
+        Task transportation = new Task("transpotation",root);
+
+        root.addComponent(softwareDesign);
+        root.addComponent(softwareTesting);
+        root.addComponent(dataBase);
+        root.addComponent(transportation);
+*/
+
+
+
+
+    private static Component feedChildren(JSONObject o, Component comp) {
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        if(o.getString("Class").equals("class Task")){
+            JSONArray intervals = o.getJSONArray("Intervals");
+
+            comp= new Task(o.getString("Name"));
+            System.out.println(comp.getTagName());
+
+            if(o.getJSONArray("Intervals")!=null){
+
+                ((Task) comp).setIntervalList(feedInterval(intervals, ((Task) comp)));
+
+            }
+        }
+        else{
+            comp =new Project(o.getString("Name"));
+            JSONArray children = o.getJSONArray("children");
+            System.out.println("tengo hijos"+ children.length());
+            for (int i = 0; i < children.length(); i++) {
+                Component auxComponent=null;
+                ((Project) comp).addComponent(feedChildren(children.getJSONObject(i),auxComponent));
+                //(feedChildren());
+            }
+
+
+
+
+
+            //System.out.println(comp.getTagName());
+
+        }
+
+        comp.setDuration(Duration.ofSeconds(o.getLong("Duration")));
+        comp.setInitialDate(LocalDateTime.parse(o.getString("InitialDate"), formatter));
+        comp.setDateFinal(LocalDateTime.parse(o.getString("FinalDate"), formatter));
+
+
+
+
+        //comp.setTagName(o.getString("Name"));
+        //comp.setTagName("algo");
+
+
+
+        //o.
+
+
+        return comp;
+    }
+//TODO Solucionar SetInterval.
+    private static List<Interval> feedInterval(JSONArray addIntervals, Task task) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+
+        List<Interval> intervals = new ArrayList<>();
+
+
+        task.addInterval();
+        Interval auxInterval = new Interval(task);
+        for (int i = 0; i < addIntervals.length(); i++) {
+            //auxInterval.setFinalDate(addIntervals.get());
+
+            //intervals.add(i, );
+
+            auxInterval.setDuration(Duration.ofSeconds(addIntervals.getLong(Integer.parseInt("Duration"))));
+            auxInterval.setInitialDate(LocalDateTime.parse(addIntervals.getString(Integer.parseInt("InitialDate")), formatter));
+            auxInterval.setFinalDate(LocalDateTime.parse(addIntervals.getString(Integer.parseInt("FinalDate")), formatter));
+            intervals.add(auxInterval);
+        }
+
+        return intervals;
+
+    }
+
     public static void crearJSON(){
         JSONObject obj= new JSONObject();
         obj.put("root", writeTreeToJSON(root));
@@ -140,6 +288,7 @@ public class Main {
         return jo;
     }
 
+
     public static JSONObject writeChildrenRoot(Component childrenComponent){
         DateTimeFormatter DATEFORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         JSONObject itemChildren = new JSONObject();
@@ -153,6 +302,7 @@ public class Main {
         itemChildren.put("FinalDate", (childrenComponent.getDateFinal()==null) ? "null" : childrenComponent.getDateFinal().format(DATEFORMATTER));
         itemChildren.put("Duration", childrenComponent.getDuration().toSeconds());
 
+        //childrenComponent.getChildrenProject();
         if(childrenComponent instanceof Task){
             for (Interval i: ((Task) childrenComponent).getIntervalList()){
                 listInterval.put(writeIntervalsTask(i));
