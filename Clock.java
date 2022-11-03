45f84d1c128b1505a4ce3a0e0f8b3e5ca4d9953e
Lluis161200 <73023169+Lluis161200@users.noreply.github.com>
2022-11-03 23:18:10 +0100

    comentarios de la clase clock

--- Clock.java
+++ Clock.java
@@ -1,18 +1,25 @@
 import java.util.Observable;
 import java.util.Timer;
 import java.util.TimerTask;
-
+/**
+ * The class Clock inherits from observable. It contains a NotifyTask class that will notify the observer
+ */
 public class Clock extends Observable {
     private int seconds;
     private static Clock instanceClock;
     private static Timer timer;
-
+    /**
+     * The constructor of the clock class
+     * @param seconds Every seconds that clock will notify to its observer
+     */
     private Clock(int seconds) {
         setSeconds(seconds);
         setTimer(new Timer());
         timer.scheduleAtFixedRate(new NotifyTask(),0, this.seconds *1000);
        }
-
+    /**
+     * Implementation of Singleton
+     */
     public static synchronized Clock getInstanceClock(int seconds){
         if(instanceClock ==null){
             instanceClock =new Clock(seconds);
@@ -34,7 +41,9 @@ public class Clock extends Observable {
     }
     public class NotifyTask extends TimerTask{
 
-
+        /**
+         * Inner class to notify the observers
+         */
 
         @Override
         public void run() {
