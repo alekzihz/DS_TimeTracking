098e0db62e96557238f49e8e44e5587cf5bc1a80
Alexis <57765568+alekzihz@users.noreply.github.com>
2022-11-22 10:52:00 +0100

    comentarios en interface visitor

--- Visitor.java
+++ Visitor.java
@@ -1,3 +1,6 @@
+/**
+ * visitor interface that visit projects,tasks and intervals that provides a template printer/searchTag
+ * */
 public interface Visitor {
     public void visitTask(Task task);
     public void visitProject(Project project);
